98be33a02884d56c78f7ac11342fba047e8764ce
naXeer rehman <naxeerrehman@gmail.com>
2024-08-05 11:48:59 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,34 +1,22 @@
-import React from "react";
-// import clock
-import "@leenguyen/react-flip-clock-countdown";
-// import clock css
-import "@leenguyen/react-flip-clock-countdown/dist/index.css";
-import FlipClockCountdown from "@leenguyen/react-flip-clock-countdown";
-// import video
-import videoBg from "../src/assets/video.mp4";
+import React from 'react'
+import '@leenguyen/react-flip-clock-countdown'
+import '@leenguyen/react-flip-clock-countdown/dist/index.css'
+import videoBg from '../src/assets/video.mp4'
+import FlipClockCountdown from '@leenguyen/react-flip-clock-countdown'
 const App = () => {
   return (
-    <section className="page">
-      {/* overlay */}
-      <div className="overlay"></div>
-      {/* video */}
-      <video src={videoBg} autoPlay loop muted></video>
-      {/* content */}
-      <div className="page__content">
-        <h1>Launching Soon</h1>
-        <h3>Leave your email and we'll let you know once the site goes live</h3>
-        {/* clock */}
-        <FlipClockCountdown
-          to={new Date().getTime() + 24 * 3600 * 1000 + 5000}
-          className="flip-clock"
-          Labels={["days", "HOURS", "MINUTES", "SECONDS"]}
-          duration={0.5}
-        />
-        {/* button */}
-        <button className="btn">Notify Me</button>
-      </div>
+    <section className='page'>
+<video src={videoBg} autoPlay muted loop></video>
+<div className='page__content'>
+  <h1>Launching soon</h1>
+  <h3>Leave your email and we'll let you know once the site goes live</h3>
+  <FlipClockCountdown
+  to={new Date().getTime()+24*3600*1000+5000}
+  className='flip-clock'
+/>
+<button className='btn'>Notify me</button>
+</div>
     </section>
-  );
-};
-
-export default App;
+  )
+}
+export default App
